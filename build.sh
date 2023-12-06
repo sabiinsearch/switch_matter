ea95c1611252badae2139dbc951b09e6e318c0fa
sabiinsearch <sabiinsearch@gmail.com>
2023-12-06 20:14:47 +0530

    update

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@
 cd /home/sarbjeetsingh/matter
 source scripts/activate.sh
 
+
 # Build project
 cd /home/sarbjeetsingh/github/Projects/Hukam_projects/matter/switch_matter
 gn gen out/debug --args="ti_sysconfig_root=\"$HOME/ti/sysconfig_1.15.0\""
