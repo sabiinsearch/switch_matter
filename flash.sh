1a9129ad0edba2ff7e6fead093eaa74be2ec1ea2
sabiinsearch <sabiinsearch@gmail.com>
2023-12-26 18:44:45 +0530

    update

--- flash.sh
+++ flash.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+echo "Approaching uniflash directory"
 
 cd /home/sarbjeetsingh/ti/uniflash_8.3.0/
 
-./dslite.sh --config=CC2652R7.ccxml -f -v /home/sarbjeetsingh/github/Projects/Hukakm_projects/matter/switch_matter/out/debug/chip-LP_CC2652R7-lighting-example-bim.hex
+echo "flashing..."
+
+./dslite.sh --config=CC2652R7.ccxml -f -v /home/sarbjeetsingh/github/Projects/Hukam_projects/matter/switch_matter/out/debug/chip-LP_CC2652R7-lighting-example-bim.hex
 
 echo "Done"
